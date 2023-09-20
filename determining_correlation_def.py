fc11ca284a3c736417853ff90b99d6dbf4af47f8
dartveider19 <micle.mk29111999@gmail.com>
2023-09-20 19:24:07 +0200

    Update determining_correlation_def.py

--- determining_correlation_def.py
+++ determining_correlation_def.py
@@ -11,6 +11,10 @@ import numpy as np
 ##name = the name of the file in that folder
 ###change backslashes to slashes
 
+
+#extracts data from the GEO file (only downloaded file)
+## name = name of the file
+## path = direct path to the folder where file is located
 def data_unpack(name, path):
     global data
 
@@ -28,6 +32,7 @@ def data_unpack(name, path):
 
     return data
 
+#filters control genes out of the main df
 def data_controls ():
     global controls
 
@@ -35,6 +40,7 @@ def data_controls ():
 
     return controls
 
+#filters positive controls out of the controls df
 def data_controls_pos ():
     global controls_pos
 
@@ -42,6 +48,7 @@ def data_controls_pos ():
 
     return controls_pos
 
+#filters negative controls out of the controls df
 def data_controls_neg():
     global controls_neg
 
@@ -49,6 +56,7 @@ def data_controls_neg():
 
     return controls_neg
 
+#returns statistical description of all negative controls
 def controls_neg_stat_desc():
     global neg_contr_st_desc
     global neg_contr_clean
@@ -59,6 +67,8 @@ def controls_neg_stat_desc():
 
     return neg_contr_clean, neg_contr_st_desc
 
+
+#returns statistical description of the positive controls
 def controls_pos_stat_desc():
     global pos_contr_st_desc
     global pos_contr_clean
@@ -70,6 +80,7 @@ def controls_pos_stat_desc():
 
     return pos_contr_clean, pos_contr_st_desc
 
+#extracts data for the main genes (filter out all non main genes)
 def data_main ():
     global main_expr
 
@@ -77,6 +88,7 @@ def data_main ():
 
     return main_expr
 
+#returns only genes with positive expression lvl compared to control genes
 def neg_sample_comparison ():
 
     global st_sig_genes
@@ -94,8 +106,9 @@ def neg_sample_comparison ():
     return st_sig_genes, st_sig_genes_clean
 
 
-
-def gene_assignment ():
+#takes argument 'target' = every data that need to be assigned by ID_REF
+##mozna dodac jeszcze zmienna by = zeby mozna bylo wybrac wg czego dopasowywac
+def gene_assignment (target):
     global gene_data_assigned
 
     gene_data = data.loc[:, ['ID_REF', 'gene_assignment']]
@@ -109,10 +122,85 @@ def gene_assignment ():
     gene_data['gene_name'] = gene_names
     gene_data['gene_name'] = gene_data['gene_name'].str.strip()
     gene_data = gene_data.drop('gene_assignment', axis='columns')
-    gene_data_assigned = st_sig_genes_clean.merge(gene_data, on=['ID_REF'], how='left')
+    gene_data_assigned = target.merge(gene_data, on=['ID_REF'], how='left')
     first_index = gene_data_assigned.pop('gene_name')
     gene_data_assigned.insert(1, 'gene_name', first_index)
 
     return gene_data_assigned
 
+#returns on;y genes with negative expression
+def neg_sample_comparison_neg_expr ():
+
+    global st_sig_genes_neg_expr
+    global st_sig_genes_clean_neg_expr
+
+    st_sig_genes_neg_expr = data.loc[:, ['ID_REF']]
+    for (gsm, gsm_data) in neg_contr_st_desc.iteritems():
+        local_main_expr = main_expr.loc[:, ['ID_REF', gsm]]
+        st_sig_genes_local = local_main_expr.loc[local_main_expr[gsm] < neg_contr_st_desc.at['95%', gsm]]
+        #st_sig_genes = pd.concat([st_sig_genes, st_sig_genes_local], axis=1)
+        st_sig_genes_neg_expr= st_sig_genes_neg_expr.merge(st_sig_genes_local, on=['ID_REF'], how='outer')
+
+    st_sig_genes_clean_neg_expr = st_sig_genes_neg_expr.dropna()
+
+    return st_sig_genes_neg_expr, st_sig_genes_clean_neg_expr
+
+
+#takes all genes and assignes their epression levels to the 95% of control expression level
+def determining_expr_lvl ():
+
+    global data_expr_lvl
+
+    data_expr_lvl = main_expr.loc[:, ['ID_REF']]
+    for (gsm, gsm_data) in neg_contr_st_desc.iteritems():
+        local_main_expr = main_expr.loc[:, ['ID_REF', gsm]]
+        values = []
+        for a in local_main_expr[gsm]:
+            a -= neg_contr_st_desc.at['95%', gsm]
+            values.append(a)
+        local_main_expr[gsm] = values
+        #st_sig_genes = pd.concat([st_sig_genes, st_sig_genes_local], axis=1)
+
+        data_expr_lvl= data_expr_lvl.merge(local_main_expr, on=['ID_REF'], how='outer')
+
+    return data_expr_lvl
+
+def correlation (gene_name):
+    global correlation_data
+
+    coor_test_data = data_expr_lvl
+    coor_test_data = coor_test_data.set_index('ID_REF').T
+    gene_id = gene_data_assigned.loc[gene_data_assigned['gene_name'] == gene_name, 'ID_REF'].iloc[0]
+    gene = coor_test_data[gene_id]
+    pearson_improved = []
+    pearson_p_value = []
+    spearman_improved = []
+    spearman_p_value = []
+    kendall_improved = []
+    kendall_p_value = []
+    for a in coor_test_data:
+        i = coor_test_data[a]
+        pearson_improved.append(scipy.stats.pearsonr(i, gene)[0])
+        pearson_p_value.append(scipy.stats.pearsonr(i, gene)[1])
+        spearman_improved.append(scipy.stats.spearmanr(i, gene)[0])
+        spearman_p_value.append(scipy.stats.spearmanr(i, gene)[1])
+        kendall_improved.append(scipy.stats.kendalltau(i, gene)[0])
+        kendall_p_value.append(scipy.stats.kendalltau(i, gene)[1])
+        print(a)
+        print(rho, cor, tau)
+    correlation_data = pd.DataFrame()
+    correlation_data['ID_REF'] = data_expr_lvl['ID_REF']
+    correlation_data['Pearson r'] = pearson_improved
+    correlation_data['Pearson p-value'] = pearson_p_value
+    correlation_data['Spearman rho'] = spearman_improved
+    correlation_data['Spearman p-value'] = spearman_p_value
+    correlation_data['Kendall tau'] = kendall_improved
+    correlation_data['Kendall p-value'] = kendall_p_value
+    gene_assignment_local = gene_data_assigned.loc[:, ['ID_REF', 'gene_name']]
+    correlation_data = correlation_data.merge(gene_assignment_local, on='ID_REF', how='left')
+    first_index = correlation_data.pop('gene_name')
+    correlation_data.insert(1, 'gene_name', first_index)
+
+
+    return correlation_data
 
